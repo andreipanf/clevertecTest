49d0bac97e6c6a64d24a34d2cdfba666a849308b
Andrey Panfilov <andreipanf@gmail.com>
2015-11-21 16:09:00 +0700

    Initial Commit

---
+++ clevertecTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  clevertecTest
+//
+//  Created by Andrey on 21.11.15.
+//  Copyright © 2015 Andrey. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
